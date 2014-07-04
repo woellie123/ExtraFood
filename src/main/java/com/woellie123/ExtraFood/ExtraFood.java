980c59682e49ad9d593ee0500d513a0c16e7af0a
woellie123 <woellie123@gmail.com>
2014-07-04 16:48:05 +0200

    Upload

---
+++ src/main/java/com/woellie123/ExtraFood/ExtraFood.java
@@ -0,0 +1,32 @@
+package com.woellie123.ExtraFood;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ExtraFood", name="Extra Food", version="1.7.10-1.0")
+
+public class ExtraFood
+{
+    @Mod.Instance("ExtraFood")
+    public static ExtraFood instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/com/woellie123/ExtraFood/proxy/CommonProxy.java
@@ -0,0 +1,6 @@
+package com.woellie123.ExtraFood.proxy;
+
+public class CommonProxy implements IProxy
+{
+
+}
