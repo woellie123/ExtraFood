cf2f07b098ad43fc467fb18c7cccf24397e2b4b2
woellie123 <woellie123@gmail.com>
2014-07-16 19:22:01 +0200

    Updated to recommended forge version

--- src/main/java/com/woellie123/ExtraFood/ExtraFood.java
+++ src/main/java/com/woellie123/ExtraFood/ExtraFood.java
@@ -1,6 +1,6 @@
 package com.woellie123.ExtraFood;
 
-import com.woellie123.ExtraFood.configuration.ConfigurationHandler;
+import com.woellie123.ExtraFood.handler.ConfigurationHandler;
 import com.woellie123.ExtraFood.proxy.IProxy;
 import com.woellie123.ExtraFood.reference.Reference;
 import cpw.mods.fml.common.Mod;
@@ -9,7 +9,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 
 public class ExtraFood
 {
---
+++ src/main/java/com/woellie123/ExtraFood/client/gui/ModGuiConfig.java
@@ -0,0 +1,21 @@
+package com.woellie123.ExtraFood.client.gui;
+
+import com.woellie123.ExtraFood.handler.ConfigurationHandler;
+import com.woellie123.ExtraFood.reference.Reference;
+import cpw.mods.fml.client.config.GuiConfig;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.common.config.Configuration;
+
+public class ModGuiConfig extends GuiConfig
+{
+    public ModGuiConfig(GuiScreen guiScreen)
+    {
+        super(guiScreen,
+                new ConfigElement(ConfigurationHandler.configuration.getCategory(Configuration.CATEGORY_GENERAL)).getChildElements(),
+                Reference.MOD_ID,
+                false,
+                false,
+                GuiConfig.getAbridgedConfigPath(ConfigurationHandler.configuration.toString()));
+    }
+}
