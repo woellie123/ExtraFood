bcc2235aca7920a980fe8c368adf43fbf69665c6
woellie123 <woellie123@gmail.com>
2014-07-16 19:59:48 +0200

    Create ConfigurationHandler.java

---
+++ src/main/java/com/woellie123/ExtraFood/handler/ConfigurationHandler.java
@@ -0,0 +1,41 @@
+package com.woellie123.ExtraFood.handler;
+
+import com.woellie123.ExtraFood.reference.Reference;
+import cpw.mods.fml.client.event.ConfigChangedEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.common.config.Configuration;
+
+import java.io.File;
+
+public class ConfigurationHandler
+{
+    public static Configuration configuration;
+    public static boolean testValue = false;
+
+    public static void init(File configFile)
+    {
+        if (configuration == null)
+        {
+            configuration = new Configuration(configFile);
+            loadConfiguration();
+        }
+    }
+
+    @SubscribeEvent
+    public void onConfigurationChangedEvent(ConfigChangedEvent event)
+    {
+        if (event.modID.equalsIgnoreCase(Reference.MOD_ID))
+        {
+            loadConfiguration();
+        }
+    }
+
+    private static void loadConfiguration()
+    {
+        //                                    //name        //category                      //default //text
+        testValue = configuration.getBoolean("configValue", configuration.CATEGORY_GENERAL, false, "This is an example configuration value");
+
+        if (configuration.hasChanged())
+            configuration.save();
+    }
+}
